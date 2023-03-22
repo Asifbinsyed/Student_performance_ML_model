741905b346d8de89dda4bf0cc78ee55357f3bd75
Asif bin Syed <Asifbinsyed33@gmail.com>
2023-03-22 12:11:32 -0400

    data ingestion

---
+++ src/exception.py
@@ -0,0 +1,18 @@
+import sys 
+
+def error_message_detail(error,error_detail:sys):
+    _,_exc_tb= error_detail.exec_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message= "error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+    
+
+class CustomException(Exception):
+    def_init_(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
